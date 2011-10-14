9cc626c38897e04421dd4bdceca90a855f8e833e
nickrak <nickrak.nr@gmail.com>
2011-10-13 20:34:15 -0700

    Fixed redstone event check to check around instead of at.

--- nickrak/RedstoneSpawners/RedstoneListener.java
+++ nickrak/RedstoneSpawners/RedstoneListener.java
@@ -1,28 +1,51 @@
 package nickrak.RedstoneSpawners;
 
-import org.bukkit.Location;
+import java.util.ArrayList;
+
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
-import org.bukkit.entity.CreatureType;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockRedstoneEvent;
 
 public class RedstoneListener extends BlockListener
-{   
+{
     @Override
     public void onBlockRedstoneChange(BlockRedstoneEvent event)
     {
-        final Block b = event.getBlock();
-        if (b.getType() == Material.MOB_SPAWNER && event.getNewCurrent() > 0 && event.getOldCurrent() == 0)
+        if (event.getOldCurrent() != 0 || event.getNewCurrent() == 0)
         {
-            final CreatureSpawner cs = (CreatureSpawner) b.getState();
-            final CreatureType ct = cs.getCreatureType();
-            final World w = b.getWorld();
-            final Location l = b.getLocation().add(0, 1, 0);
-            
-            w.spawnCreature(l, ct);
+            return;
         }
+        
+        final Block b = event.getBlock();
+        final ArrayList<Block> sb = new ArrayList<Block>(6);
+        sb.add(b.getRelative(0, 1, 0));
+        sb.add(b.getRelative(0, -1, 0));
+        sb.add(b.getRelative(0, 0, 1));
+        sb.add(b.getRelative(0, 0, -1));
+        sb.add(b.getRelative(1, 0, 0));
+        sb.add(b.getRelative(-1, 0, 0));
+        
+        for (final Block tb : sb)
+        {
+            spawnerAct(tb);
+        }
+        
+        /*final CreatureSpawner cs = (CreatureSpawner) b.getState();
+        final CreatureType ct = cs.getCreatureType();
+        final World w = b.getWorld();
+        final Location l = b.getLocation().add(0, 1, 0);
+
+        w.spawnCreature(l, ct);*/
+    }
+    
+    private final void spawnerAct(final Block b)
+    {
+        if (b.getType().getId() == Material.MOB_SPAWNER.getId())
+        {
+            CreatureSpawner cs = (CreatureSpawner) b.getState();
+            b.getWorld().spawnCreature(b.getLocation().add(0, 1, 0), cs.getCreatureType());
+        }        
     }
 }
